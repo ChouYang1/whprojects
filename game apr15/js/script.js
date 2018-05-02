f19d8b6e7d709d4396b2f6123d94d1abefa2fc5b
ChouYang1 <chouyang@mail.fresnostate.edu>
2018-05-02 09:05:54 -0700

    game update may2

--- game apr15/js/script.js
+++ game apr15/js/script.js
@@ -1,31 +1,7 @@
-function cheatcode() {
-    var code = prompt("Please enter cheat code");
-    var clue = 44;
-    if(code == clue){
-        alert("You are the champion cheater! Claim your LVL 1 prize at Geekwise Academy.  LVL up skip stage 1.");
-
-    }else {
-            alert("Wrong, Proceed to dice rolling!");
-        }
-}
-
-function cheatcode1() {
-    var code = prompt("Please enter cheat code");
-    var clue = "127.0.0.1";
-    if(code == clue){
-        alert("You are the champion cheater! Claim your LVL 2 prize at Geekwise Academy.  LVL up skip stage 2.");
-
-    }else {
-            alert("Wrong, Proceed ");
-        }
-}
-
 
 function rolldice(){
     var dice1 = document.getElementById("dice1");
     var dice2 = document.getElementById("dice2");
-    //var odd = [3,5,7,9,11];
-    //var even =[2,4,6,8,10,12];
     var status = document.getElementById("status");
     var dice01 = Math.floor(Math.random() * 6) + 1;
     var dice02 = Math.floor(Math.random() * 6) + 1;
@@ -38,19 +14,59 @@ function rolldice(){
     status.innerHTML += " Winning! Doubles!  You get a free roll!";
     }else if(dicetotal < 6){
             status.innerHTML += " < You win, lvl up!";
+            return true;
         }else {
             status.innerHTML += " > Loser, game over.";
+            addEventListener("click", reload());
         }
     }
-    //}else if(dicetotal == even){
-    //     status.innerHTML += " You win, lvl up!";
-        
-    // }else {
-    //     status.innerHTML += " You lose, game over.";
-        
-    // }
-    
-function lvl2(){
+
+
+function cheatcode() {
+    var code = prompt("Enter cheat code");
+    var clue = 44;
+    console.log(code);
+    if(code == clue){
+        alert("You are the champion cheater! Claim your LVL 1 prize at Geekwise Academy.  LVL up skip stage 1.");
+        return(true);
+        }else
+            alert("Wrong, Proceed to dice rolling!");
+            addEventListener("click", rolldice());
+                console.log(dicetotal);
+               
+            
+            
+        }
+
+function unlock2(){
+function cheatcode1() {
+    var code1 = prompt("Please enter cheat code");
+    var clue1 = '127.0.0.1';
+    if(code1 == clue1){
+        alert("You are the champion cheater! Claim your LVL 2 prize at Geekwise Academy.  You are now the Geekwise Master!  You can now add your Stage 3 code.");
+
+    }else {
+            alert("Wrong, Proceed to game. ");
+            
+        }
+}
+}
+function unlock(){
+    if(false)
+    rolldice();
+
 
 }
+
+// function (num){
+//     if(num < rand){
+//         alert(num + ' is too low. Pick again');
+//         pickAnotherNum();
+//     }else if(num > rand){
+//         alert(num + ' is too high. Pick again');
+//         pickAnotherNum();
+//     }else {
+//         alert(num + ' is the right number!! You win!');
+//     }
+// }
 
